308b95959e1ce5d60b6618c1e5a36619f9d45d11
Bryan Rodriguez <Serphp@hotmail.com>
2022-10-21 07:39:54 -0500

    Update script ? add 3 new functions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const screen = document.querySelectorAll('.screen');
 const start_btn = document.getElementById('.start-btn');
 
-let seconds = 0;
+
 let score = 0;
 let select_point = {};
 
@@ -15,12 +15,40 @@ choose_point_btns.forEeach((btn) => {
         const src = img.getAttribute('src');
         const alt = img.getAttribute('alt');
         select_point = { src, alt };
+        //cambio de pantalla
         screen[1].classList.add('up');
         setTimeout(createPoint, 1000);
         startGame();
     });
 });
 
-function start(){
+function startGame(){
     setInterval(increaseTime, 1000);
+}
+// variables de incremento de tiempo
+let seconds = 0;
+const Tiempo = document.getElementById('time');
+//
+function increaseTime(){
+    let step1 = Math.floor(seconds / 60);
+    let step2 = seconds % 60;
+    //Operador condicional ternario
+    step1 = step1 < 10 ? `0${step1}` : step1;
+    step2 = step2 < 10 ? `0${step2}` : step2;
+    Tiempo.innerHTML = `Tiempo: ${step1}:${step2}`;
+    seconds++;
+}
+
+function createPoint(){
+    const point = document.createElement('div')
+    //addpoint
+    point.classList.add('point');
+    const {x, y} = getrlocation()
+    point.style.top = `${y}px`;
+    point.style.left = `${x}px`;
+    point.innerHTML = `<img src="${select_point.src}" alt="${select_point.alt}" style="transform: rotate(${Math.random() * 360}deg)" />`;
+}
+
+function getrlocation(){
+
 }
