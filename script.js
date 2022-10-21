9499d8de344d31cbd0f4b8d79de4ba75aeed22ca
Bryan Rodriguez <Serphp@hotmail.com>
2022-10-21 02:13:17 -0500

    update index>add selectors points

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const screen = document.querySelectorAll('.screen');
+const start_btn = document.getElementById('.start-btn');
+
 let seconds = 0;
 let score = 0;
 let select_point = {};
+
+start_btn.addEventListener('click', () => {
+    screen[0].classList.add('up');
+});
+
+choose_point_btns.forEeach((btn) => {
+    btn.addEventListener('click', () => {
+        const img = btn.querySelector('img');
+        const src = img.getAttribute('src');
+        const alt = img.getAttribute('alt');
+        select_point = { src, alt };
+        screen[1].classList.add('up');
+        setTimeout(createPoint, 1000);
+        startGame();
+    });
+});
+
+function start(){
+    setInterval(increaseTime, 1000);
+}
